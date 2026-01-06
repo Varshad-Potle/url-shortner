6516a0b81a9f88e13acd725fbf6e7d9d50bf36d8
Varshad-Potle <work.varshad@gmail.com>
2026-01-06 16:42:13 +0530

    Setup supabase backend

---
+++ src/db/supabase.js
@@ -0,0 +1,13 @@
+// This file initializes and configures a Supabase client instance
+// It sets up the connection to Supabase using environment variables for the URL and API key
+// The configured client is exported as the default export to be used by other parts of the application
+// Supabase is a PostgreSQL database with real-time capabilities and built-in authentication
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
+        
