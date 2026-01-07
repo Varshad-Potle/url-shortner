3447d41120c8522905ac1039144aec3517ab9ec9
Varshad-Potle <work.varshad@gmail.com>
2026-01-07 16:05:11 +0530

    defining routes and building header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import LandingPage from "./pages/landing";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import Link from "./pages/link";
+import RedirectLink from "./pages/redirect-link";
 
-  return (
-    <div className="text-2xl bg-red-500">Hello world</div>
-  )
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/link/:id",
+        element: <Link />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+      {
+        path: "*",
+        element: <div>404 - Page Not Found</div>,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
